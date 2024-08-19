7a692585815262df0a3053d61e108cfbcfadb5a0
yeohongred <yeohongred@gmail.com>
2024-08-19 16:31:37 +0800

    Add generate_text() to edit_video.py

--- edit_video.py
+++ edit_video.py
@@ -5,7 +5,12 @@ import random
 import pyttsx3
 from pedalboard import Pedalboard, Reverb
 from pedalboard.io import AudioFile
+from PIL import Image, ImageDraw, ImageFont
 from moviepy.editor import *
+
+
+WIDTH = 1080
+HEIGHT = 1920
 
 
 # 0 is male voice, 1 is female voice (for my computer)
@@ -22,7 +27,6 @@ def generate_audio(voice=0, text="The quick brown fox jumps over the lazy dog.",
     engine.runAndWait()
 
 
-# might need to add normalisation
 def process_audio(input_file="generate_audio_output.wav", output_file="process_audio_output.wav"):
     board = Pedalboard([Reverb(room_size=0.4, damping=1, wet_level=0.2, dry_level=0.3)])
 
@@ -32,6 +36,18 @@ def process_audio(input_file="generate_audio_output.wav", output_file="process_a
                 chunk = input_audio.read(input_audio.samplerate)
                 effected = board(chunk, input_audio.samplerate, reset=False)
                 output_audio.write(effected)
+
+
+def generate_text():
+    text = "The quick brown fox jumps over the lazy dog."
+    text_image = Image.new('RGBA', (WIDTH, HEIGHT), color=(0, 0, 0, 0))
+
+    text_image_draw = ImageDraw.Draw(text_image)
+    text_image_draw.font = ImageFont.truetype("assets/montserrat_semibold.ttf")
+
+    text_image_draw.text(xy=(10, 10), text=text, fill=(255, 255, 0))
+
+    text_image.save('media/generate_text_output.png')
 
 
 def generate_video(gameplay_video_file="minecraft.mp4", background_audio_file="the_begin.wav", voice_audio_file="process_audio_output.wav", output_file="generate_video_output.mp4"):
@@ -55,6 +71,4 @@ def generate_video(gameplay_video_file="minecraft.mp4", background_audio_file="t
 
 
 if __name__ == "__main__":
-    generate_audio()
-    process_audio()
-    generate_video()
+    generate_text()
