046e58441beabed5ff9340a25cbb4c7c7b96d755
yeohongred <yeohongred@gmail.com>
2024-08-25 00:19:46 +0800

    Revamp generate_video to create minimum viable product

--- edit_video.py
+++ edit_video.py
@@ -59,36 +59,59 @@ def generate_text(text_list=["The quick brown fox jumps over the lazy dog", "Lor
         if file.endswith(".png"):
             os.remove(os.path.join(f"media/{output_directory}", file))
 
-    # Save each text to its own file
+    # Save each text to its own .png file
     for i, text in enumerate(text_list):
+        # Set up image
         text_image = Image.new("RGBA", (WIDTH, HEIGHT), color=(0, 0, 0, 0))
         text_image_draw = ImageDraw.Draw(text_image)
 
+        # Draw text in the centre of the screen
         _, _, text_width, text_height = text_image_draw.textbbox((0, 0), text, font=FONT)
         text_image_draw.text(((WIDTH-text_width)/2, (HEIGHT-text_height)/2), text, font=FONT, fill=(255, 255, 255))
 
+        # Save to .png file
         text_image.save(f"media/{output_directory}/{output_directory}_{i}.png")
 
 
-def generate_video(gameplay_video_file="minecraft.mp4", background_audio_file="the_begin.wav", voice_audio_file="process_audio_output.wav", output_file="generate_video_output.mp4"):
-    gameplay_video = VideoFileClip(f"assets/{gameplay_video_file}")
-    background_audio = AudioFileClip(f"assets/{background_audio_file}")
-    voice_audio = AudioFileClip(f"media/{voice_audio_file}")
+def generate_video(gameplay_video_file=None, text_image_directory="generate_text_output", background_audio_file=None, voice_audio_directory="process_audio_output", output_file="generate_video_output"):
+    # Randomly choose gameplay footage if not provided
+    if gameplay_video_file == None:
+        for _, _, files in os.walk("assets"):
+            gameplay_video_files = [file for file in files if file.endswith(".mp4")]
+        gameplay_video_file = random.choice(gameplay_video_files)
+    
+    # Randomly choose background music if not provided
+    if background_audio_file == None:
+        for _, _, files in os.walk("assets"):
+            background_audio_files = [file for file in files if file.endswith(".wav")]
+        background_audio_file = random.choice(background_audio_files)
 
-    video_length = math.ceil(voice_audio.duration)
+    # Set up clips
+    gameplay_video = VideoFileClip(f"assets/{gameplay_video_file}")
+    text_images = [ImageClip(f"media/{text_image_directory}/{text_image_directory}_{i}.png") for i in range(sum((len(files) for _, _, files in os.walk(f"media/{text_image_directory}"))))]
+    background_audio = AudioFileClip(f"assets/{background_audio_file}")
+    voice_audios = [AudioFileClip(f"media/{voice_audio_directory}/{voice_audio_directory}_{i}.wav") for i in range(sum((len(files) for _, _, files in os.walk(f"media/{voice_audio_directory}"))))]
+
+    # Sequence text with voice audio
+    for i in range(len(text_images)):
+        text_images[i] = text_images[i].set_duration(voice_audios[i].duration).set_audio(voice_audios[i])
+    text_with_voice = concatenate_videoclips(text_images)
+
+    # Trim gameplay video to correct length
+    video_length = math.ceil(sum(voice_audio.duration for voice_audio in voice_audios))
     start_time = random.randint(0, math.floor(gameplay_video.duration) - video_length)
     gameplay_video = gameplay_video.subclip(start_time, start_time + video_length)
 
+    # Reduce volume of background audio
     background_audio = background_audio.subclip(0, video_length)
     background_audio = background_audio.volumex(0.05)
 
-    final_video = gameplay_video
-    final_audio = CompositeAudioClip([background_audio, voice_audio])
-
+    # Organise and combine video and audio, save to .mp4 file
+    final_video = CompositeVideoClip([gameplay_video, text_with_voice])
+    final_audio = CompositeAudioClip([background_audio, text_with_voice.audio])
     final_output = final_video.set_audio(final_audio)
-    os.makedirs("media", exist_ok=True)
-    final_output.write_videofile(f"media/{output_file}", fps=60)
+    final_output.write_videofile(f"media/{output_file}.mp4", fps=60)
 
 
 if __name__ == "__main__":
-    generate_text()
+    generate_video()
